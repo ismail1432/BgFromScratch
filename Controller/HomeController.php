cc7715f4e44b9b2f4d4fe5fd2d1141dce3a8ce91
ismail1432 <smaine.milianni@gmail.com>
2017-04-25 23:00:04 +0200

    Validator class added but not complete and not tested yet...

--- Controller/HomeController.php
+++ Controller/HomeController.php
@@ -31,6 +31,11 @@ class HomeController extends Controller implements BaseController
 
     public function showPost($view, $id)
     {
+        if(isset($_POST) && !empty($_POST)){
+
+            new Validator($_POST);
+
+        }
         $db = parent::databaseConnect();
         $post = New ArticleManager($db);
         $post = $post->getArticle($id);
--- Validator/Validator.php
+++ Validator/Validator.php
@@ -15,6 +15,8 @@ namespace BgFromScratch\Validator;
  */
 class Validator
 {
+    public  $errorTabs = [];
+
     function __construct(Array $datas)
     {
 
@@ -23,36 +25,38 @@ class Validator
             $this->isCommentTooShort($datas['content']);
             $this->isPseudoTooShort($datas['auteur']);
             $this->isPseudoLetters($datas['auteur']);
+            //die(var_dump($this->errorTabs));
     }
 
     public function isEmpty($value){
         if(empty($value)){
-            return 'Veuillez remplir tous les champs ! ';
+            return $this->errorTabs[] += 'Veuillez remplir tous les champs ! ';
         }
     }
 
     public function isPseudoTooShort($value){
         if(strlen($value) < 3){
-            return 'Le pseudo est trop court, il faut 3 caractères minimum';
+            return $this->errorTabs[] += 'Le pseudo est trop court, il faut 3 caractères minimum';
         }
     }
 
     public function isCommentTooShort($value){
         if(strlen($value) < 10){
-            return 'Le commentaire est trop court il faut 10 caractères minimum';
+            return $this->errorTabs[] += 'Le commentaire est trop court il faut 10 caractères minimum';
         }
         if(strlen($value) > 150){
-            return 'Le commentaire est trop long il faut 150 caractères minimum';
+            return $this->errorTabs[] += 'Le commentaire est trop long il faut 150 caractères minimum';
         }
     }
 
     public function isPseudoLetters($value){
-        if(preg_match("^[\s]+$",$value)){
-            return 'Le pseudo ne peut pas contenir d\'espace !';
+        if(preg_match("/^[\s]+$/",$value)){
+             $this->errorTabs[] += 'Le pseudo ne peut pas contenir d\'espace !';
         }
-        if(preg_match("^[\d]+$", $value)){
-            return 'Le pseudo ne peut pas contenir que des chiffres !';
+        if(preg_match("/^[\d]+$/", $value)){
+            $this->errorTabs[] += 'Le pseudo ne peut pas contenir que des chiffres !';
         }
+        return $this->errorTabs;
     }
 
 }
